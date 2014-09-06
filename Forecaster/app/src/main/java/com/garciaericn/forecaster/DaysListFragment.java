4bd58c48107320b944c90eb8d295fad24afea3be
eng618 <eng618@fullsail.edu>
2014-09-06 15:30:04 -0400

    Added 2 fragment classes for the list (extends ListFragment)  and details (extends Fragment)

---
+++ Forecaster/app/src/main/java/com/garciaericn/forecaster/DaysListFragment.java
@@ -0,0 +1,12 @@
+package com.garciaericn.forecaster;
+
+import android.app.ListFragment;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 9/6/14.
+ */
+public class DaysListFragment extends ListFragment {
+
+}
---
+++ Forecaster/app/src/main/java/com/garciaericn/forecaster/WeatherDetailsFragment.java
@@ -0,0 +1,12 @@
+package com.garciaericn.forecaster;
+
+import android.app.Fragment;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 9/6/14.
+ */
+public class WeatherDetailsFragment extends Fragment {
+
+}
