65eacacdb24202f87c240a5a114975466993e9bd
eng618 <eng618@fullsail.edu>
2014-09-12 23:54:15 -0400

    Created SettingsActivity

---
+++ Forecaster/app/src/main/java/com/garciaericn/forecaster/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.garciaericn.forecaster;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 9/12/14.
+ */
+public class SettingsActivity extends PreferenceActivity {
+
+}
