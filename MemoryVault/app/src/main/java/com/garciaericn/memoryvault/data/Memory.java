6640fe026de2061a63b3ffeba9eae0809825cac9
eng618 <eng618@fullsail.edu>
2014-09-16 01:05:15 -0400

    Created stub for Memory object.

---
+++ MemoryVault/app/src/main/java/com/garciaericn/memoryvault/data/Memory.java
@@ -0,0 +1,12 @@
+package com.garciaericn.memoryvault.data;
+
+import java.io.Serializable;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 9/16/14.
+ */
+public class Memory implements Serializable {
+    public static final long serialVersionUID = 2357817694738294783L;
+}
