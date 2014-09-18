104128c5d033f7bf76d276ac6b0a1dff3f9a00ed
eng618 <eng618@fullsail.edu>
2014-09-18 00:35:16 -0400

    Added code to manager class to check/read/write to disk.

--- MemoryVault/app/src/main/java/com/garciaericn/memoryvault/data/MemoryManager.java
+++ MemoryVault/app/src/main/java/com/garciaericn/memoryvault/data/MemoryManager.java
@@ -1,5 +1,9 @@
 package com.garciaericn.memoryvault.data;
 
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
 import java.util.HashMap;
 
 /**
@@ -9,10 +13,12 @@ import java.util.HashMap;
  */
 public class MemoryManager {
 
+    private static final String TAG = "MemoryManager.TAG";
     public HashMap <Long, Memory> memories;
 
     // Initializes memories HashMap if null
     public MemoryManager() {
+        Log.i(TAG, "MemoryManager Created");
         if (memories == null) {
             memories = new HashMap<Long, Memory>();
         }
@@ -20,15 +26,74 @@ public class MemoryManager {
 
     // Adds given memeory
     public void addMemory(Memory memory) {
+        Log.i(TAG, "addMemory entered");
         memories.put(memory.getMemoryKey(), memory);
     }
 
     // Removes a given memory
     public void removeMemory(Memory memory) {
+        Log.i(TAG, "removeMemory entered");
         memories.remove(memory.getMemoryKey());
     }
 
-    public void saveToDisk() {
+    private Boolean checkFile (Context context, String fileName) {
+        Log.i(TAG, "checkFile entered");
+        // Store data in "protected" directory
+        File external = context.getExternalFilesDir(null);
+        File file = new File(external, fileName);
+        return file.exists();
+    }
+
+    public void writeToDisk(Context context, Memory memory) {
+        Log.i(TAG, "writeToDisk entered");
         // TODO: Add save code
+//        // Store data in "protected" directory
+//        File external = context.getExternalFilesDir(null);
+//        File file = new File(external, fileName);
+//
+//        try {
+//            //Create new output stream
+//            FileOutputStream fos = new FileOutputStream(file);
+//            // Convert string to byte and write to stream
+//            fos.write(data.getBytes());
+//            // Close the stream to sve the file
+//            fos.close();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    private String readFromDisk(String fileName) {
+        Log.i(TAG, "readFromFile entered");
+        // TODO: Read data from saved file
+
+//        String savedWeatherString = null;
+//
+//        File external = getExternalFilesDir(null);
+//        File file = new File(external, fileName);
+//
+//        try {
+//            FileInputStream fis = new FileInputStream(file);
+//            // Create stream readers
+//            InputStreamReader inReader = new InputStreamReader(fis);
+//            BufferedReader reader = new BufferedReader(inReader);
+//
+//            // Read data and pass to StringBuffer
+//            StringBuilder buffer = new StringBuilder();
+//            String text;
+//            // Make sure a line of text is available to be read
+//            while ((text = reader.readLine()) != null) {
+//                buffer.append(text);
+//            }
+//            Log.i(TAG, "String from file: " + buffer.toString());
+//
+//            savedWeatherString = buffer.toString();
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return savedWeatherString;
+        return null;
     }
 }
