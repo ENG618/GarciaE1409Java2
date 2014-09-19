4bd6491eb2eedcee99240b8685ce53cf7e395814
eng618 <eng618@fullsail.edu>
2014-09-19 06:42:44 -0400

    Defined MemoryDetailActivity.  Created menu xml for details activity.

--- MemoryVault/app/src/main/java/com/garciaericn/memoryvault/MemoryDetailsActivity.java
+++ MemoryVault/app/src/main/java/com/garciaericn/memoryvault/MemoryDetailsActivity.java
@@ -1,6 +1,9 @@
 package com.garciaericn.memoryvault;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.garciaericn.memoryvault.fragments.MemoryDetailsFragment;
 
 /**
  * Full Sail University
@@ -8,4 +11,28 @@ import android.app.Activity;
  * Created by ENG618-Mac on 9/19/14.
  */
 public class MemoryDetailsActivity extends Activity {
+
+    public static final String TAG = "MemoryDetailsActivity.TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memory_details);
+
+        if (savedInstanceState == null) {
+            // Create fragment
+            MemoryDetailsFragment fragment = new MemoryDetailsFragment();
+            // Obtain bundle from intent
+            Bundle b = getIntent().getBundleExtra(MemoryListActivity.MEMORYBUNDLE);
+            fragment.setArguments(b);
+
+            //Load new fragment
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.detail_fragment_container, fragment, MemoryDetailsFragment.TAG);
+        } else {
+            // TODO: Load from savedInstanceState
+        }
+
+    }
 }
--- MemoryVault/app/src/main/java/com/garciaericn/memoryvault/fragments/MemoryDetailsFragment.java
+++ MemoryVault/app/src/main/java/com/garciaericn/memoryvault/fragments/MemoryDetailsFragment.java
@@ -1,9 +1,59 @@
 package com.garciaericn.memoryvault.fragments;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.garciaericn.memoryvault.R;
+import com.garciaericn.memoryvault.data.Memory;
+
 /**
  * Full Sail University
  * Mobile Development BS
  * Created by ENG618-Mac on 9/19/14.
  */
-public class MemoryDetailsFragment {
+public class MemoryDetailsFragment extends Fragment {
+    public static final String TAG = "MemoryDetailsFragment.TAG";
+
+    private Memory memory;
+
+    public MemoryDetailsFragment() {}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate entered");
+
+        Bundle b = getArguments();
+        if (b != null && b.containsKey(Memory.EVENT_KEY)) {
+            memory = new Memory(b);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView entered");
+        //Load layout
+        View view = inflater.inflate(R.layout.fragment_memory_details, container, false);
+
+        if (memory != null) {
+            // Obtain and set all fields
+            TextView eventTV = (TextView) view.findViewById(R.id.eventNameTV);
+            eventTV.setText(memory.getEventName());
+
+            TextView guestsTV = (TextView) view.findViewById(R.id.guestsTV);
+            guestsTV.setText(memory.getNumGuests());
+
+            TextView locationTV = (TextView) view.findViewById(R.id.locationTV);
+            locationTV.setText(memory.getEventLocation());
+
+            TextView notesTV = (TextView) view.findViewById(R.id.notesTV);
+            eventTV.setText(memory.getEventNotes());
+        }
+        return view;
+    }
 }
