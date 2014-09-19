d07faf793fc07609f265a86917d559a0234d5fdd
eng618 <eng618@fullsail.edu>
2014-09-19 06:19:11 -0400

    Added Detail activity/fragment/xml

---
+++ MemoryVault/app/src/main/java/com/garciaericn/memoryvault/MemoryDetailsActivity.java
@@ -0,0 +1,11 @@
+package com.garciaericn.memoryvault;
+
+import android.app.Activity;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 9/19/14.
+ */
+public class MemoryDetailsActivity extends Activity {
+}
---
+++ MemoryVault/app/src/main/java/com/garciaericn/memoryvault/fragments/MemoryDetailsFragment.java
@@ -0,0 +1,9 @@
+package com.garciaericn.memoryvault.fragments;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 9/19/14.
+ */
+public class MemoryDetailsFragment {
+}
