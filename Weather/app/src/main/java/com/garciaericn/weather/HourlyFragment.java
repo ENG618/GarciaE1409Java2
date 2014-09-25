bf1c16ed901cf9fc648072ee793ff5be52c5b092
eng618 <eng618@fullsail.edu>
2014-09-25 02:06:44 -0400

    [Weather] Set up three fragments and respective layouts for each tab view.

---
+++ Weather/app/src/main/java/com/garciaericn/weather/HourlyFragment.java
@@ -0,0 +1,11 @@
+package com.garciaericn.weather;
+
+import android.app.ListFragment;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 9/25/14.
+ */
+public class HourlyFragment extends ListFragment {
+}
---
+++ Weather/app/src/main/java/com/garciaericn/weather/WeeklyFragment.java
@@ -0,0 +1,9 @@
+package com.garciaericn.weather;
+
+/**
+ * Full Sail University
+ * Mobile Development BS
+ * Created by ENG618-Mac on 9/25/14.
+ */
+public class WeeklyFragment {
+}
