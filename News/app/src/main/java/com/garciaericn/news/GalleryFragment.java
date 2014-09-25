76f0bfd2b61b3ddf3f605c33b0bd5023d28e0211
eng618 <eng618@fullsail.edu>
2014-09-25 04:16:04 -0400

    [News] Updated GalleryFragment to populate correct view.

--- News/app/src/main/java/com/garciaericn/news/GalleryFragment.java
+++ News/app/src/main/java/com/garciaericn/news/GalleryFragment.java
@@ -1,7 +1,11 @@
 package com.garciaericn.news;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Full Sail University
@@ -28,5 +32,19 @@ public class GalleryFragment extends Fragment{
         args.putInt(ARG_SECTION_NUMBER, sectionNumber);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_gallery, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
     }
 }
